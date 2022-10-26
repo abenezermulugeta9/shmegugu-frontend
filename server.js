92e89e414509424f39679131255a7304d103c186
AbenezerM <abenezermulugeta41@gmail.com>
2022-10-26 14:25:14 -0500

    Add node js server to bootup the angular app

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static(__dirname+'/dist/frontend'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname+'/dist/frontend/index.html')
+});
+
+app.listen(process.env.PORT || 8080);
